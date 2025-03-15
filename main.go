1c77129c35be3408b78a8aeec8bb24a88ad73922
Christopher Herzog <chjherzog@gmail.com>
2025-03-15 11:37:31 +0100

    Add CLI structure

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -14,10 +16,65 @@ func CleanInput(text string) []string {
 	return loweredText
 }
 
-func main() {
-	cleanedText := CleanInput("  hello    World")
-	for i := range cleanedText {
-		fmt.Println(cleanedText[i])
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp(allCliCommands map[string]cliCommand) error {
+	fmt.Println("Available commands")
+	for _, command := range allCliCommands {
+		fmt.Sprintf("%s: %s", command.name, command.description)
 	}
-	fmt.Println(cleanedText)
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func main() {
+	allCliCommands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exits the program",
+			callback:    commandExit,
+		},
+	}
+	allCliCommands["help"] = cliCommand{
+		name:        "help",
+		description: "Exits the program",
+		callback:    func() error { return commandHelp(allCliCommands) },
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage: ")
+	for {
+		fmt.Println()
+		for _, val := range allCliCommands {
+			fmt.Println(val.name + ": " + val.description)
+		}
+		scanner.Scan()
+		line := CleanInput(scanner.Text())
+
+		if len(line) == 0 {
+			continue
+		}
+
+		firstWord := line[0]
+
+		command, exists := allCliCommands[firstWord]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		if err := command.callback(); err != nil {
+			fmt.Println("Error: ", err)
+		}
+	}
 }
